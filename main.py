b03123a4d00161c544f7a7796ff8c602d25337f7
newpaxonian <newpaxonian@gmail.com>
2022-08-30 17:08:47 -0500

    adding spaceships

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import pygame as pg
+import os
 
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
-pg.display.set_caption("Trash Game")
+YELLOW_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pg.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+RED_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pg.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+
+pg.display.set_caption('Trash Game')
 
 
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP,
+             (WIDTH / 4 - YELLOW_SPACESHIP.get_width() / 4,
+              HEIGHT / 4 - YELLOW_SPACESHIP.get_height() / 4)
+             )
+    WIN.blit(RED_SPACESHIP,
+             (WIDTH * 3 / 4 - RED_SPACESHIP.get_width() * 3 / 4 ,
+              HEIGHT * 3 / 4 - RED_SPACESHIP.get_height() * 3 / 4)
+             )
     # pg.display.update()
 
 
