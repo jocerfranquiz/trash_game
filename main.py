18e34c2c9ce0e997d6fa712cfb9c0f96ab0979d6
newpaxonian <newpaxonian@gmail.com>
2022-08-31 02:02:43 -0500

    correcting spaceships position and orientation

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ WHITE = (255, 255, 255)
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pg.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+YELLOW_SPACESHIP = pg.transform.rotate(
+    pg.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
 
 RED_SPACESHIP_IMAGE = pg.image.load(os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pg.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT))
+RED_SPACESHIP = pg.transform.rotate(
+    pg.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), -90)
 
 pg.display.set_caption('Trash Game')
 
@@ -20,16 +22,18 @@ def draw_window():
     WIN.fill(WHITE)
     WIN.blit(YELLOW_SPACESHIP,
              (WIDTH / 4 - YELLOW_SPACESHIP.get_width() / 4,
-              HEIGHT / 4 - YELLOW_SPACESHIP.get_height() / 4)
+              HEIGHT / 2 - YELLOW_SPACESHIP.get_height() / 2)
              )
     WIN.blit(RED_SPACESHIP,
-             (WIDTH * 3 / 4 - RED_SPACESHIP.get_width() * 3 / 4 ,
-              HEIGHT * 3 / 4 - RED_SPACESHIP.get_height() * 3 / 4)
+             (WIDTH * 3 / 4 - RED_SPACESHIP.get_width() * 3 / 4,
+              HEIGHT / 2 - RED_SPACESHIP.get_height() / 2)
              )
     # pg.display.update()
 
 
 def main():
+
+    # TODO: https://youtu.be/jO6qQDNa2UY?t=1690
     clock = pg.time.Clock()
     run = True
     while run:
